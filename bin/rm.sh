b7830f5f00f749ac63849ae852b6af6620ff8ee7
Kael <i@kael.me>
2013-07-06 01:34:36 +0800

    support verbose; support -ri; support combined short options; deal with error exit code; manage global exit code; support dir exam

--- bin/rm.sh
+++ bin/rm.sh
@@ -2,16 +2,17 @@
 
 TRASH="$HOME/.Trash"
 COMMAND="$0"
-Y=`date +%Y`
-M=`date +%m`
-D=`date +%d`
-h=`date +%H`
-m=`date +%M`
-s=`date +%S`
-TIME="$Y-$M-$D $h.$m.$s"
+
+GUID=0
+TIME=
+date_time(){
+    TIME=$(date +%Y-%m-%d-%H-%M-%S)-$GUID
+    (( GUID += 1 ))
+}
+
 
 # flags
-debug=
+debug=1
 
 # tools
 debug(){
@@ -20,20 +21,24 @@ debug(){
     fi
 }
 
-invalid_option(){
-    # -k -> k
-    c=`echo $1 | cut -d '-' -f2`
 
-    # todo
-    echo "rm: illegal option -- $c"
+# parse argv -----------------------------
+
+invalid_option(){
+    # if there's an invalid option, `rm` only takes the second char of the option string
+    # case:
+    # rm -c
+    # -> rm: illegal option -- c
+    echo "rm: illegal option -- ${1:1:1}"
     usage
 }
 
-
-usage()
-{
+usage(){
     echo "usage: rm [-f | -i] [-dPRrvW] file ..."
     echo "       unlink file"
+
+    # if has an invalid option, exit with 64
+    exit 64
 }
 
 
@@ -42,53 +47,131 @@ if [[ "$#" = 0 ]]; then
     usage
 fi
 
-# overload arguments
 
+ARG_END=
 FILE_NAME=
+ARG=
+
+file_i=0
+arg_i=0
+
+split_push_arg(){
+    # remove leading '-' and split combined short options
+    # -vif -> vif -> v, i, f
+    split=`echo ${1:1} | fold -w1`
+
+    local arg
+    for arg in ${split[@]}
+    do
+        ARG[arg_i]="-$arg"
+        ((arg_i += 1))
+    done
+}
+
+push_arg(){
+    ARG[arg_i]=$1
+    ((arg_i += 1))
+}
+
+push_file(){
+    FILE_NAME[file_i]=$1
+    ((file_i += 1))
+}
+
+# pre-parse argument vector
+while [ -n "$1" ]
+do
+    # case:
+    # rm -v abc -r --force
+    # -> -r will be ignored
+    # -> args: ['-v'], files: ['abc', '-r', 'force']
+    if [[ -n "$ARG_END" ]]; then
+        push_file $1
+
+    else
+        case $1 in
+
+            # case:
+            # rm -v -f -i a b
+
+            # case:
+            # rm -vf -ir a b
+
+            # ATTENSION: 
+            # Regex in bash is not perl regex,
+            # in which `'*'` means at least one and has no `'+'`
+            -[a-zA-Z]*)
+                split_push_arg $1; debug "short option $1"
+                ;;
+
+            # rm --force a
+            --[a-zA-Z]*)
+                push_arg $1; debug "option $1"
+                ;;
+
+            # rm -- -a
+            --)
+                ARG_END=1; debug "divider"
+                ;;
+
+            # case:
+            # rm -
+            # -> args: [], files: ['-']
+            *)
+                push_file $1; debug "file $1"
+                ARG_END=1
+                ;;
+        esac
+    fi
+
+    shift
+done
 
 # flags
 OPT_FORCE=
 OPT_INTERACTIVE=
 OPT_RECURSIVE=
+OPT_VERBOSE=
 
-# temp
-i=0
+# global exit code, default to 0
+EXIT_CODE=0
 
-while [ -n "$1" ]
+# parse options
+for arg in ${ARG[@]}
 do
-    case $1 in
-        # -[hH]|--[hH]elp)
+    debug arg $arg
+    case $arg in
+
+        # There's no --help|-h option for rm on Mac OS 
+        # [hH]|--[hH]elp)
         # help
         # shift
         # ;;
 
         -f|--force)
-        OPT_FORCE=1; debug "force"
-        shift
-        ;;
+            OPT_FORCE=1; debug "force"
+            ;;
 
         -i|--interactive)
-        OPT_INTERACTIVE=1; debug "interactive"
-        shift
-        ;;
+            OPT_INTERACTIVE=1; debug "interactive"
+            ;;
 
+        # both r and R is allowed
         -[rR]|--[rR]ecursive)
-        OPT_RECURSIVE=1; debug "recursive"
-        shift
-        ;;
+            OPT_RECURSIVE=1; debug "recursive"
+            ;;
 
-        -[a-zA-Z0-9]*)
-        invalid_option $1
-        shift
-        ;;
+        # only lowercase v is allowed
+        -v|--verbose)
+            OPT_VERBOSE=1; debug "verbose"
+            ;;
 
         *)
-        FILE_NAME[i]=$1; debug "file: $1"
-        ((i += 1))
-        shift
-        ;;
+            invalid_option $arg
+            ;;
     esac
 done
+# /parse argv -----------------------------
 
 
 # make sure recycled bin exists
@@ -105,58 +188,100 @@ if [[ ! -e "$TRASH" ]]; then
     fi
 fi
 
+# try to remove a file or directory
+remove(){
+    local file=$1
 
-for file in ${FILE_NAME[@]}
-do
-    file_basename=`basename $file`
-    trash_name="$TRASH/$file_basename"
-    fail=0
-
-    # file
     if [[ -e "$file" ]]; then
-        if [[ -d "$file" ]]; then
-
-            # if a directory, and without '-r' option
-            if [[ ! -n "$OPT_RECURSIVE" ]]; then
-                echo "$COMMAND: $file: is a directory"
-                echo "(maybe you should use a \"-r\" option)"
-                fail=1
-            fi
-        fi
-    else
         echo "$COMMAND: $file: No such file or directory"
-        fail=1
+        exit 1
     fi
 
+    if [[ -d "$file" ]]; then
 
-    if [[ "$fail" = 0 ]]; then
-
-        # if already in the trash
-        if [[ -e "$trash_name" ]]; then
-            trash_name="$trash_name $TIME"
-            # echo "exists, rename to $trash_name"
-
-        else
-            trash_name="$trash_name"
+        # if a directory, and without '-r' option
+        if [[ ! -n "$OPT_RECURSIVE" ]]; then
+            echo "$COMMAND: $file: is a directory"
+            exit 1
         fi
 
-        # echo "move $file"
-        # echo "to $trash_name"
-
         if [[ "$OPT_INTERACTIVE" = 1 ]]; then
-            echo "remove $file? "
+            echo -n "examine files in directory $file? "
             read answer
 
-            if [[ "$answer" = "yes" || "$answer" = "y" || "$answer" = "YES" || "$answer" = "Y" ]]; then
+            # actually, as long as the answer start with 'y', the file will be removed
+            # default to no remove
+            if [[ ${anwser:0:1} =~ [yY] ]]; then
+
+                # if choose to examine the dir, recursively check files first
+                recursive_remove $file
+
+                # interact with the dir at last
+                echo -n "remove $file? "
+                read answer
+                if [[ ${anwser:0:1} =~ [yY] ]]; then
+                    if [[ $(ls -A $file) ]] && trash $file || {
+                        echo "$COMMAND: $file: Directory not empty"
+
+                        # set global exit code
+                        EXIT_CODE=1
+                    }
+                fi
+            fi
+        else
+            trash $file
+        fi
+
+    else
+        if [[ "$OPT_INTERACTIVE" = 1 ]]; then
+            echo -n "remove $file? "
+            read answer
+            if [[ ${anwser:0:1} =~ [yY] ]]; then
                 :
             else
                 exit 0
             fi
         fi
 
-        mv "$file" "$trash_name"
+        trash $file
     fi
+}
+
+
+recursive_remove(){
+    local path
+
+    for path in $1/*
+    do
+        remove $path
+    done
+}
+
+
+# determine to trash
+trash(){
+    local file=$1
+    trash_name=$TRASH/$(basename $file)
+
+    # if already in the trash
+    if [[ -e "$trash_name" ]]; then
+        # renew $TIME
+        date_time
+        trash_name="$trash_name $TIME"
+    fi
+
+    [[ "$OPT_VERBOSE" = 1 ]] && echo $file
+
+    mv "$file" "$trash_name"
+}
+
+
+for file in ${FILE_NAME[@]}
+do
+    # remove the ending '/'
+    # abc/abc/ -> abc/abc
+    remove ${file%/}
 done
 
-
+exit $EXIT_CODE
 
